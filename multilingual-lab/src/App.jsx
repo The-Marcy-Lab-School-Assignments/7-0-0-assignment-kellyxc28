b2a48484a0066e017e307ccafe7d1841ea3b26c0
Kelly Xiong Chen <kellyxiongc@gmail.com>
2024-08-08 14:27:31 -0400

    Completed: Changing Font Size When Button Clicked

--- multilingual-lab/src/App.jsx
+++ multilingual-lab/src/App.jsx
@@ -1,9 +1,30 @@
 // useState is imported at the top
-import { useState } from 'react';
+// the following line is for checking 
+// import { useEffect, useState } from 'react';
+import {useState } from 'react';
 
 // component names use PascalCase
-const WelcomeDisplay = ({ welcomePhrase }) => {
-  return <p>{welcomePhrase}</p>
+const WelcomeDisplay = ({ fontSize, welcomePhrase }) => {
+  // useEffect(()=> {
+  //   console.log(fontSize)
+  // }, [])
+  return (
+    <>
+      <p style={{fontSize: `${fontSize}px`}}> {welcomePhrase}</p>
+    </>
+  )
+}
+
+const FontSizeButtons = ({ increment, decrement }) => {
+  // props are destructured ^          ^
+  
+  // multiple returned components are wrapped with () and <> </>
+  return (
+    <>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+    </>
+  )
 }
 
 const LanguageButtons = ({ english, chinese, spanish, haitian, portuguese }) => {
@@ -24,8 +45,13 @@ const LanguageButtons = ({ english, chinese, spanish, haitian, portuguese }) => 
 // App is exported (default or named is fine)
 const App = () => {
   // state is "lifted up" and passed down with props
+  const [fontSize, setFontSize] = useState(20);
   const [welcomePhrase, setLang] = useState('Welcome!');
   
+  // helper functions can be passed down instead of the setter function itself
+  const increment = () => { setFontSize(fontSize + 1) }
+  const decrement = () => { setFontSize(fontSize - 1) }
+
   // helper functions can be passed down instead of the setter function itself
   const english = () => { setLang('Welcome!') }
   const chinese = () => { setLang('欢迎!') }
@@ -35,7 +61,8 @@ const App = () => {
 
   return (
     <>
-      <WelcomeDisplay welcomePhrase={welcomePhrase} />
+      <WelcomeDisplay fontSize={fontSize} welcomePhrase={welcomePhrase} />
+      <FontSizeButtons increment={increment} decrement={decrement} />
       <LanguageButtons english={english} chinese={chinese} spanish={spanish} haitian={haitian} portuguese={portuguese} />
     </>
   )
