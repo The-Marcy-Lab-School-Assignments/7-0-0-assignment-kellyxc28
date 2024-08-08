77fa065024f7981a8359b950136557370efbb75e
Kelly Xiong Chen <kellyxiongc@gmail.com>
2024-08-08 14:01:14 -0400

    Completed: Replacing Text Based On Language Clicked

--- multilingual-lab/src/App.jsx
+++ multilingual-lab/src/App.jsx
@@ -2,18 +2,21 @@
 import { useState } from 'react';
 
 // component names use PascalCase
-const CounterDisplay = ({ count }) => {
-  return <p>{count}</p>
+const WelcomeDisplay = ({ welcomePhrase }) => {
+  return <p>{welcomePhrase}</p>
 }
 
-const CounterButtons = ({ increment, decrement }) => {
+const LanguageButtons = ({ english, chinese, spanish, haitian, portuguese }) => {
   // props are destructured ^          ^
   
   // multiple returned components are wrapped with () and <> </>
   return (
     <>
-      <button onClick={increment}>+</button>
-      <button onClick={decrement}>-</button>
+      <button onClick={english}>English</button>
+      <button onClick={chinese}>Chinese</button>
+      <button onClick={spanish}>Spanish</button>
+      <button onClick={haitian}>Haitian</button>
+      <button onClick={portuguese}>Portuguese</button>
     </>
   )
 }
@@ -21,16 +24,19 @@ const CounterButtons = ({ increment, decrement }) => {
 // App is exported (default or named is fine)
 const App = () => {
   // state is "lifted up" and passed down with props
-  const [count, setCount] = useState(0);
-
+  const [welcomePhrase, setLang] = useState('Welcome!');
+  
   // helper functions can be passed down instead of the setter function itself
-  const increment = () => { setCount(count + 1) }
-  const decrement = () => { setCount(count - 1) }
+  const english = () => { setLang('Welcome!') }
+  const chinese = () => { setLang('欢迎!') }
+  const spanish = () => { setLang('Bienvenida!') }
+  const haitian = () => { setLang('Benvenuta!') }
+  const portuguese = () => { setLang('Bem-vindo!') }
 
   return (
     <>
-      <CounterDisplay count={count} />
-      <CounterButtons increment={increment} decrement={decrement} />
+      <WelcomeDisplay welcomePhrase={welcomePhrase} />
+      <LanguageButtons english={english} chinese={chinese} spanish={spanish} haitian={haitian} portuguese={portuguese} />
     </>
   )
 }
